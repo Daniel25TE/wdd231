a71fe83644f19f67d45a958ef647853c8736602e
Daniel25TE <danielalejandrosud25@gmail.com>
2025-06-20 13:20:37 -0600

    maribaos backend 1.8

--- final/scripts/thanks.js
+++
@@ -1,9 +0,0 @@
-import { setupMenuToggle } from './modules/menuToggle.js';
-import { thanksdetails } from './modules/thanksdetails.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    setupMenuToggle();
-    thanksdetails();
-
-});

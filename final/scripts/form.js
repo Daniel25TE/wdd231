07a35fb07ad61345779dedd40aaa9fba510d3ff4
Daniel25TE <danielalejandrosud25@gmail.com>
2025-06-20 13:21:04 -0600

    maribaos backend 1.8

---
+++ final/scripts/form.js
@@ -0,0 +1,10 @@
+import { dataForm } from './modules/dataForm.js';
+
+import { setupMenuToggle } from './modules/menuToggle.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    dataForm()
+    setupMenuToggle();
+
+});
